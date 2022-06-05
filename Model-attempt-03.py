7164206b44c491090a5db6828e0c6d881145cc6b
Aleks B <50184059+thesixtium@users.noreply.github.com>
2022-06-05 12:28:52 -0600

    Update Model-attempt-03.py

--- Model-attempt-03.py
+++ Model-attempt-03.py
@@ -1,74 +1,26 @@
-# Params
-'''
-{
-    "all_wheels_on_track": Boolean,        # flag to indicate if the agent is on the track
-    "closest_waypoints": [int, int],       # indices of the two nearest waypoints.
-    "distance_from_center": float,         # distance in meters from the track center
-    "is_crashed": Boolean,                 # Boolean flag to indicate whether the agent has crashed.
-    "is_left_of_center": Boolean,          # Flag to indicate if the agent is on the left side to the track center or not.
-    "is_offtrack": Boolean,                # Boolean flag to indicate whether the agent has gone off track.
-    "is_reversed": Boolean,                # flag to indicate if the agent is driving clockwise (True) or counter clockwise (False).
-    "heading": float,                      # agent's yaw in degrees
-    "progress": float,                     # percentage of track completed
-    "speed": float, (0.0:5.0)              # agent's speed in meters per second (m/s)
-    "steering_angle": float,               # agent's steering angle in degrees
-    "steps": int,                          # number steps completed
-    "track_length": float,                 # track length in meters.
-    "track_width": float,                  # width of the track
-    "waypoints": [(float, float), ]        # list of (x,y) as milestones along the track center
-
-}
-
-Params to include:
-waypoints > speed > borders
-.7 > .2 > .1
-'''
-
-import math
-
 def reward_function(params):
-    reward = 1e-3
-    if params['is_crashed']:
-        return reward
-
-    # Read input variables
-    waypoints = params['waypoints']
-    closest_waypoints = params['closest_waypoints']
-    heading = params['heading']
-    speed = params['speed'] / 5
-    all_wheels_on_track = params['all_wheels_on_track']
+    # Read input parameters
     distance_from_center = params['distance_from_center']
+    is_offtrack = params['is_offtrack']
+    progress = params['progress']
+    steps = params['steps']
     track_width = params['track_width']
 
-    # Init Rewards
-    waypoint_reward = 1.0  # Initialize the reward with typical value
-    borders_reward = 1e-3  # Give a very low reward by default
+    # No going off track
+    if is_offtrack:
+        reward = 0.001
+        return float(reward)
 
-    # Waypoints
-    # Calculate the direction of the centerline based on the closest waypoints
-    next_point = waypoints[closest_waypoints[1]]
-    prev_point = waypoints[closest_waypoints[0]]
+    # Encourage staying in the center
+    track_width_half = track_width / 2
+    quadratic_center = (distance_from_center / track_width_half) ** 4
+    distance_from_center_reward = 1 - quadratic_center
+    normalized_distance_from_center_reward = 0.5 + (distance_from_center_reward / 2)
 
-    # Calculate the direction in radius, arctan2(dy, dx), the result is (-pi, pi) in radians
-    track_direction = math.atan2(next_point[1] - prev_point[1], next_point[0] - prev_point[0])
-    # Convert to degree
-    track_direction = math.degrees(track_direction)
+    # Encourage going around the track fast
+    fast_around_track = (progress / steps)
 
-    # Calculate the difference between the track direction and the heading direction of the car
-    direction_diff = abs(track_direction - heading)
-    if direction_diff > 180:
-        direction_diff = 360 - direction_diff
+    # Final reward
+    reward = normalized_distance_from_center_reward * fast_around_track
 
-    # Penalize the reward if the difference is too large
-    DIRECTION_THRESHOLD = 10.0
-    if direction_diff > DIRECTION_THRESHOLD:
-        waypoint_reward *= 0.5
-
-    # Borders
-    # Give a high reward if no wheels go off the track and
-    # the agent is somewhere in between the track borders
-    if all_wheels_on_track and (0.5 * track_width - distance_from_center) >= 0.05:
-        borders_reward = 1.0
-
-    reward = (waypoint_reward * 0.7) + (speed * 0.2) + (borders_reward * 0.1)
     return float(reward)
